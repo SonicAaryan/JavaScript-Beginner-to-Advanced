24bac8d3d87e04ff944a49e629aa309a90d73e90
Aaryan Sonar <aaryansonar2@gmail.com>
2023-11-08 07:53:51 +0530

    Pattern solved

--- Program19.js
+++ Program19.js
@@ -2,6 +2,9 @@ const PROMPT = require('prompt-sync')()
 let num = parseInt(PROMPT("Enter the Number :"));
 let val = 1;
 for (let i = num; i >= 1; i--) {
-    num = num + ' ';
-    console.log(num);
+    let numPattern = '';
+    for (let j = 1; j <= i; j++) {
+        numPattern += j + ' ';
+    }
+    console.log(numPattern);
 }
