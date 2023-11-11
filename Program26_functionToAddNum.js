0c9485271ad70c132e6de1d840b8458e0868ce5c
Aaryan Sonar <aaryansonar2@gmail.com>
2023-11-11 18:26:25 +0530

    Functions

--- Program26_functionToAddNum.js
+++ Program26_functionToAddNum.js
@@ -1,15 +1,15 @@
 function addGivenNumbers(array) {
     let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-        sum += array[i]
+    for (let addition = 0;addition < array.length; addition++) {
+        sum += array[addition]
     }
     console.log("The Total Sum of The Given Numbers are :", sum);
 }
 const PROMPT = require('prompt-sync')()
 let array = [];
 let length = parseInt(PROMPT("Enter the Length for Array :"));
-for (let i = 0; i < length; i++) {
-    array[i] = parseInt(PROMPT("Enter the Element for Array :"));
+for (let acceptValue = 0; acceptValue < length; acceptValue++) {
+    array[acceptValue] = parseInt(PROMPT("Enter the Element for Array :"));
 }
 addGivenNumbers(array);
 
