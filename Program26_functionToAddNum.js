719eaedf739114260d127651d1c48b9e7a18205e
Aaryan Sonar <aaryansonar2@gmail.com>
2023-11-11 18:32:26 +0530

    Functions

--- Program26_functionToAddNum.js
+++ Program26_functionToAddNum.js
@@ -2,6 +2,7 @@ function addGivenNumbers(array) {
     let sum = 0;
     for (let addition = 0;addition < array.length; addition++) {
         sum += array[addition]
+        console.log(sum);
     }
     console.log("The Total Sum of The Given Numbers are :", sum);
 }
