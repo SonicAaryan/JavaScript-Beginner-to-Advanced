2bc28714fb46b903f001334d4cad2929664dc773
Aaryan Sonar <aaryansonar2@gmail.com>
2023-11-11 17:04:40 +0530

    Updated

--- Program20_trianglePattern.js
+++ Program20_trianglePattern.js
@@ -1,17 +1,17 @@
 const PROMPT = require('prompt-sync')()
 let row = +PROMPT("Enter the Value to Print Triangle Pattern :");
-for (let i = 1; i <= row; i++) {
-    var numCount = i;
+for (let outerLoop = 1; outerLoop <= row; outerLoop++) {
+    var numCount = outerLoop;
     let emptySpace = '';
-    for (let j = row; j > i; j--) {
+    for (let freeSpaceLoop = row; freeSpaceLoop > outerLoop; freeSpaceLoop--) {
         emptySpace += '  ';
     }
-    for (let k = 1; k <= i; k++) {
+    for (let first_Half = 1; first_Half <= outerLoop; first_Half++) {
         emptySpace += numCount + ' ';
         numCount++
     }
     numCount -= 1
-    for (let l = 2; l <= i; l++) {
+    for (let second_Half = 2; second_Half <= outerLoop; second_Half++) {
         numCount -= 1;
         emptySpace += numCount + ' ';
     }
