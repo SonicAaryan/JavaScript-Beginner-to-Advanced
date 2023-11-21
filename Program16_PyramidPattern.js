7693b498a41b6ab92dc9ae933c4514dcde60499f
Aaryan Sonar <aaryansonar2@gmail.com>
2023-11-21 22:08:16 +0530

    Optimized

--- Program16_PyramidPattern.js
+++ Program16_PyramidPattern.js
@@ -1,15 +1,21 @@
 const PROMPT = require('prompt-sync')()
 let row = parseInt(PROMPT("Enter the Number :"));
+console.time("ForLoop Time");
 for (let outerLoop = 1; outerLoop <= row; outerLoop++) {
     let pattern = '';
     for (let blankSpaces = 1; blankSpaces <= row - outerLoop; blankSpaces++) {
-        pattern += '  ';
+        pattern += ' ';
     }
-    for (let patternFirstHalf = 1; patternFirstHalf <= outerLoop; patternFirstHalf++) {
-        pattern += '*' + ' ';
+    for (let patternFirstHalf = 1; patternFirstHalf <= outerLoop * 2 - 1; patternFirstHalf++) {
+        pattern += '*' ;
     }
-    for (let patternSecondHalf = 2; patternSecondHalf <= outerLoop; patternSecondHalf++) {
-        pattern += '*' + ' ';
-    }
+    // Method 2 Without optimization
+    // for (let patternFirstHalf = 1; patternFirstHalf <= outerLoop ; patternFirstHalf++) {
+    //     pattern += '*' + '  ';
+    // }
+    // for (let patternSecondHalf = 2; patternSecondHalf <= outerLoop; patternSecondHalf++) {
+    //     pattern += '*' + ' ';
+    // }
     console.log(pattern);
 }
+console.timeEnd("ForLoop Time"); // Calculate Execution Time
