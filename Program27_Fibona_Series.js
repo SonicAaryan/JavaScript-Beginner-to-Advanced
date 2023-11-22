fdf79c67bf6fc0e6e589d672d1c55c596d4d04fd
Aaryan Sonar <141862407+SonicAaryan@users.noreply.github.com>
2023-11-22 06:27:11 +0530

    logicImplemented

--- Program27_Fibona_Series.js
+++ Program27_Fibona_Series.js
@@ -1,11 +1,13 @@
 let fibo = (lengthForLoop) => {
-    let f0 = 0, f1 = 1
-    for (let iteration = 2; iteration <= lengthForLoop; iteration++) {
-        let f1 = (iteration - 1);
-        let f2 = (iteration - 2);
-        let result = f1 + f2
+    let f0 = 0, f1 = 1,next=0;
+console.log(f0);
+Console.log(F1);
+    for (let iteration = 2; iteration < lengthForLoop; iteration++) {
+        next = f0 + f1;
         console.log(result);
-    }
+        f0 = f1 ;
+        f1 = next; 
+      }
 }
 const PROMPT = require('prompt-sync')()
 let length = +PROMPT("Enter the length to print the Series :");
