25d5eacd8f5d42828cd59d16224f1c63fbc325d3
Aaryan Sonar <aaryansonar2@gmail.com>
2023-11-23 00:17:30 +0530

    Committed through app 1st and Then From local

--- Program27_Fibona_Series.js
+++ Program27_Fibona_Series.js
@@ -1,13 +1,13 @@
 let fibo = (lengthForLoop) => {
-    let f0 = 0, f1 = 1,next=0;
-console.log(f0);
-Console.log(F1);
+    let f0 = 0, f1 = 1, next = 0;
+    console.log(f0);
+    console.log(f1);
     for (let iteration = 2; iteration < lengthForLoop; iteration++) {
         next = f0 + f1;
-        console.log(result);
-        f0 = f1 ;
-        f1 = next; 
-      }
+        console.log(next);
+        f0 = f1;
+        f1 = next;
+    }
 }
 const PROMPT = require('prompt-sync')()
 let length = +PROMPT("Enter the length to print the Series :");
