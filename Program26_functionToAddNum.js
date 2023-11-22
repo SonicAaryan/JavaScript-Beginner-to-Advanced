54aed32409f693f1fe08d477360a231d35497ebe
Aaryan Sonar <aaryansonar2@gmail.com>
2023-11-22 23:06:49 +0530

    solved Without Using Array

--- Program26_functionToAddNum.js
+++ Program26_functionToAddNum.js
@@ -1,16 +1,31 @@
-function addGivenNumbers(array) {
-    let sum = 0;
-    for (let addition = 0;addition < array.length; addition++) {
-        sum += array[addition]
-        console.log(sum);
-    }
-    console.log("The Total Sum of The Given Numbers are :", sum);
-}
-const PROMPT = require('prompt-sync')()
-let array = [];
-let length = parseInt(PROMPT("Enter the Length for Array :"));
-for (let acceptValue = 0; acceptValue < length; acceptValue++) {
-    array[acceptValue] = parseInt(PROMPT("Enter the Element for Array :"));
-}
-addGivenNumbers(array);
+// 1st Way
+// function addGivenNumbers(array) {
+//     let sum = 0;
+//     for (let addition = 0;addition < array.length; addition++) {
+//         sum += array[addition]
+//         console.log(sum);
+//     }
+//     console.log("The Total Sum of The Given Numbers are :", sum);
+// }
+// const PROMPT = require('prompt-sync')()
+// let array = [];
+// let length = parseInt(PROMPT("Enter the Length for Array :"));
+// for (let acceptValue = 0; acceptValue < length; acceptValue++) {
+//     array[acceptValue] = parseInt(PROMPT("Enter the Element for Array :"));
+// }
+// addGivenNumbers(array);
 
+// 2nd Way
+const PROMPT = require('prompt-sync')()
+function addGivenNumbers() {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+    console.log(sum);
+}
+addGivenNumbers(1, 4, 5, 6, 8, 9, 10);
+// let length = parseInt(PROMPT("Enter the Length for to Add Digits :"));
+// for (let i = 0; i < length; i++) {
+//     addGivenNumbers(i) = +PROMPT("Enter the Number");
+// }
