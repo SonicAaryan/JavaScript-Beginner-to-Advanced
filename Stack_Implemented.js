b08a7b8785877909d1f0dbaa8ee72021bf858036
Aaryan Sonar <aaryansonar2@gmail.com>
2023-12-06 21:36:25 +0530

    size,isFull,peek_Functions Added

--- Stack_Implemented.js
+++ Stack_Implemented.js
@@ -1,19 +1,36 @@
-let stack=[];
-let stackSize=5;
+let stack = [];
+let stackSize = 5;
 
-function push(element){
-    if(stack.length==stackSize){
+function push(element) {
+    if (stack.length == stackSize) {
         console.log('Stack is Full! Can not Push Element !');
         return;
     }
     stack.push(element);
 }
-function pop(){
-    if(stack.length===0){
+function pop() {
+    if (stack.length === 0) {
         console.log('Stack is Empty! Can not pop Element !');
         return;
     }
     return stack.pop();
+}
+function size() {
+    console.log("The size of Stack is :", stack.length);
+}
+function isFull() {
+    if (stack.length === stackSize) {
+        console.log("The Stack is Full!");
+        return;
+    }
+    console.log("Stack is Not Full Yett");
+}
+function peek() {
+    if (stack.length > 0) {
+        console.log("The First Element is :",stack[0]);
+    }else{
+        console.log("Stack is Empty! Cannot Peek");
+    }
 }
 push(1);
 push(2);
@@ -26,6 +43,7 @@ pop();
 pop();
 pop();
 pop();
-pop();
-pop();
 console.log(`Stack ${stack}`);
+size();
+isFull();
+peek();
