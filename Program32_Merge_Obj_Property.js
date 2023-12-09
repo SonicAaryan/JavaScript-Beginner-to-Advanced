5b9c92e132d3783ba303fc4481b05b91c09f859f
Aaryan Sonar <aaryansonar2@gmail.com>
2023-12-09 20:27:01 +0530

    NotYetCorrect

---
+++ Program32_Merge_Obj_Property.js
@@ -0,0 +1,13 @@
+const marksList1 = {
+    "Alice": 77.0,
+    "Brian": 86.7
+}
+const marksList2 = {
+    "Arya": 92.5,
+    "Asia": 77.6
+}
+function mergedObjectProperty(marksList1, marksList2) {
+    return marksList1marksList2
+}
+const mergedTwoObjProperty = mergedObjectProperty(marksList1, marksList2);
+console.log(mergedObjectProperty);
