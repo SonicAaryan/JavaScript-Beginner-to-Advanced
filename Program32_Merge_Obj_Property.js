09f43db89146650b2efc709142bb4adee0eee3bd
Aaryan Sonar <aaryansonar2@gmail.com>
2023-12-11 14:35:56 +0530

    Corrected Code

--- Program32_Merge_Obj_Property.js
+++ Program32_Merge_Obj_Property.js
@@ -1,3 +1,4 @@
+// Approach 1 (By For Loop)
 const marksList1 = {
     "Alice": 77.0,
     "Brian": 86.7
@@ -6,10 +7,28 @@ const marksList2 = {
     "Arya": 92.5,
     "Asia": 77.6
 }
-function mergedObjectProperty(marksList1, marksList2) {
-    return marksList1, marksList2;
+console.log("First Object ->", marksList1);
+console.log("Second Object ->", marksList2);
+let mergedTwoObjProperty = {};
+for (let key in marksList1) {
+    mergedTwoObjProperty[key] = marksList1[key];
 }
-let mergedTwoObjProperty = mergedObjectProperty(marksList1, marksList2);
-for (let key in mergedTwoObjProperty) {
-    console.log("The Student is :", key, "and has got :", mergedTwoObjProperty[key]);
+for (let key in marksList2) {
+    mergedTwoObjProperty[key] = marksList2[key];
 }
+console.log("After Merging Object we get :", mergedTwoObjProperty);
+console.log("---------------------------------------------------------------------------------");
+// Approach 2 By (spread Operator)
+const nameList1 = {
+    "id1": "Riya",
+    "id2": "Rafel"
+};
+const nameList2 = {
+    "id3": "Angel",
+    "id4": "s-so"
+};
+const nameList3 = {
+    ...nameList1,
+    ...nameList2,
+};
+console.log("By spread Operator:", nameList3);
