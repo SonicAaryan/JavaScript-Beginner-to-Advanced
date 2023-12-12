294a339ab58af4586423c1385e27a1de119de862
Aaryan Sonar <aaryansonar2@gmail.com>
2023-12-12 08:25:55 +0530

    Added Second Approach

--- Program48_Balanced_Paranthesis.js
+++ Program48_Balanced_Paranthesis.js
@@ -1,3 +1,4 @@
+// Approach 1 BY (Array)
 const PROMPT = require('prompt-sync')()
 let symbolString = PROMPT("Enter the []{}() String combination to Check if it is Balanced Or Not! :");
 let openingSymbol = '([{';
@@ -19,4 +20,33 @@ function BalancedParanthesis(symbolString, openingSymbol, closingSymbol) {
         }
     }
 }
-console.log(BalancedParanthesis(symbolString, openingSymbol, closingSymbol));
+console.log(BalancedParanthesis(symbolString, openingSymbol, closingSymbol),"\n----------------------------");
+
+// Approach 2 BY (Object)
+// Approach 1 BY (Array)
+let String = PROMPT("Enter the []{}() String combination to Check if it is Balanced Or Not! :");
+let opening = '([{';
+let closing = ')]}';
+let mapping = {
+    ']': '[',
+    '}': '{',
+    ')': '(',
+};
+function BalancedParanthesis_Approach2(String, opening, closing) {
+    for (let i = 0; i < String.length; i++) {
+            let currentChar = String[i];
+        if (!mapping[String[i]]) {
+            stack.push(String[i]);
+            console.log("Adding In Stack -> ", stack);
+        } else {
+            let lastOpeningBracket = stack.pop();
+            if (mapping[currentChar] !== lastOpeningBracket) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+    return stack.length===0;
+}
+console.log(BalancedParanthesis_Approach2(String, opening, closing));
