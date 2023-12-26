996d11d81f0bcef930eb9a3246f469009e2d92d7
Aaryan Sonar <aaryansonar2@gmail.com>
2023-12-26 12:10:12 +0530

    Half Done

--- Program43_TruncateString.js
+++ Program43_TruncateString.js
@@ -1,7 +1,8 @@
-const PROMPT = require('prompt-sync')()
-let inputString = PROMPT("Enter the String :")
+//const PROMPT = require('prompt-sync')()
+let inputString ="The quick brown fox jumps over the lazy dogs"
 
 let truncateString = (inputString) => {
-    let newString=inputString.slice();
+    let newString=inputString.slice(0, 15);
+    console.log(newString);
 }
 truncateString(inputString);
