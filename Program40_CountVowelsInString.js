61136e953f148dcff417bd4e16a4c8df9afedceb
Aaryan Sonar <aaryansonar2@gmail.com>
2023-12-27 13:50:30 +0530

    Optimized and Added 2 Approach

--- Program40_CountVowelsInString.js
+++ Program40_CountVowelsInString.js
@@ -1,13 +1,29 @@
-let inputString = "Hello My Name is Aaryan Sonar"
-let count = 1;
+// Approach 1 : 
+const PROMPT = require('prompt-sync')()
+let inputString = PROMPT("Enter the String :");
+let vowelCount1 = 0;
 if (inputString) {
-    for (let emptySpace of inputString) {
-        if (emptySpace == " ") {
-            ++count;
+    for (let character of inputString.toLowerCase()) {
+        if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u') {
+            vowelCount1++;
         }
     }
-    console.log(count);
+    console.log("The Vowel Count is :", vowelCount1);
 }
 else {
     console.log("Enter the Sentence!");
+}
+
+// Approach 2 : A little Optimized way
+let newInputString = PROMPT("Enter the New String :");
+let vowelCount2 = 0;
+if (newInputString) {
+    for (let eachCharacter of newInputString) {
+        if (['a', 'e', 'i', 'o', 'u'].includes(eachCharacter.toLowerCase())) {
+            vowelCount2++;
+        }
+    }
+    console.log("The Vowel Count is :", vowelCount2);
+} else {
+    console.log("Enter the Sentence to Count the Vowels :");
 }
