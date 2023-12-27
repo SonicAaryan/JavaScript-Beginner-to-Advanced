1b14570aa5a342fe4783be31572230210b435489
Aaryan Sonar <aaryansonar2@gmail.com>
2023-12-27 15:21:48 +0530

    Corrected Code

--- Program43_TruncateString.js
+++ Program43_TruncateString.js
@@ -1,8 +1,25 @@
-//const PROMPT = require('prompt-sync')()
-let inputString ="The quick brown fox jumps over the lazy dogs"
-
+// Approach 1 :Logic Implemented
+const PROMPT = require('prompt-sync')()
+let inputString = PROMPT("Enter the String :");
+let truncateNumber = parseInt(PROMPT("Enter the Number to Display Specific Count of words :"))
 let truncateString = (inputString) => {
-    let newString=inputString.slice(0, 15);
-    console.log(newString);
+    let count = 0;
+    let newString = "";
+    if (inputString) {
+        for (let character of inputString) {
+            if (count === truncateNumber) {
+                break;
+            }
+            if (character == " ") {
+                count++;
+                newString += " ";
+            } else {
+                newString += character;
+            }
+        }
+        console.log(newString);
+    } else {
+        console.log("Enter the String");
+    }
 }
 truncateString(inputString);
