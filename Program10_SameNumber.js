3bb12c1b89f2c755390fb84c28cf5b869a8ba37c
Aaryan Sonar <aaryansonar2@gmail.com>
2023-12-28 21:42:29 +0530

    Parsed Into Int and Corrected if_else Statement

--- Program10_SameNumber.js
+++ Program10_SameNumber.js
@@ -1,10 +1,10 @@
 const PROMPT = require('prompt-sync')()
-let value1 = PROMPT("Enter 1st Value :");
-let value2 = PROMPT("Enter 2nd Value :");
-let value3 = PROMPT("Enter 3rd Value :");
-if (value1 == value2 && value2 == c) { // Here String is Comparing to String
+let value1 = +PROMPT("Enter 1st Value :");
+let value2 = +PROMPT("Enter 2nd Value :");
+let value3 = +PROMPT("Enter 3rd Value :");
+if (value1 == value2 && value2 == value3) { // Here String is Comparing to String
     console.log("Three Value are same :", 30);
-} else if (value1 == value2 || value2 == c || value1 == c) {
+} else if (value1 == value2 || value2 == value3) {
     console.log("Any Two Value is same :", 40);
 } else {
     console.log("Non-off the Value is Same", 20);
