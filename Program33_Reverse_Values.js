9528ee51a412d95a73136fb501329697cc638d3c
Aaryan Sonar <aaryansonar2@gmail.com>
2023-12-29 18:14:43 +0530

    Added Two Approach

--- Program33_Reverse_Values.js
+++ Program33_Reverse_Values.js
@@ -1,10 +1,15 @@
+// Approach 1 : Without Using Inbuilt Method();
 const PROMPT = require('prompt-sync')()
 let intArray = [];
 let length = PROMPT("Enter the Length for Array :");
 for (let indexOfArray = 0; indexOfArray < length; indexOfArray++) {
-    intArray[indexOfArray] = PROMPT("Enter the Number :");
+    intArray[indexOfArray] = +PROMPT("Enter the Number :");
 }
 console.log("<---- Printing Value by Reversing Them ---->");
 for (let reverseArray = intArray.length - 1; reverseArray >= 0; reverseArray--) {
     console.log(intArray[reverseArray]);
 }
+
+// Approach 2 : With Using inBuilt()
+let numArray = [10, 20, 30, 4];
+console.log("Using inBuilt Method :", numArray.reverse());
