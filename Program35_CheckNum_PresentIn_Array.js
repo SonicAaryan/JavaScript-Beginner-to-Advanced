5ed743039331ab8f0a9bdeba7985eaf64386379d
Aaryan Sonar <aaryansonar2@gmail.com>
2023-12-29 18:45:59 +0530

    Corrected The Issue

--- Program35_CheckNum_PresentIn_Array.js
+++ Program35_CheckNum_PresentIn_Array.js
@@ -1,17 +1,19 @@
 const PROMPT = require('prompt-sync')()
-let length = PROMPT("Enter the Length for Array :");
+let length = +PROMPT("Enter the Length for Array :");
 let intArray = [length];
 for (let arrayIndex = 0; arrayIndex < length; arrayIndex++) {
-    intArray[arrayIndex] = PROMPT("Enter the Value :");
+    intArray[arrayIndex] = +PROMPT("Enter the Value :");
 }
 let numCheck = parseInt(PROMPT("Enter the Number to check if it is Present in Array or Not :"));
-function NumberCheck(numCheck) {
+let flag = false;
+function NumberCheck(intArray, numCheck) {
+    let newArray = intArray;
     for (let arrayIndex = 0; arrayIndex < length; arrayIndex++) {
-        if (intArray[arrayIndex] === length) {
-            return true;
-        } else {
-            return false;
+        if (newArray[arrayIndex] === numCheck) {
+            flag = true;
+            break;
         }
     }
+    console.log(flag);
 }
-console.log(NumberCheck(numCheck));
+NumberCheck(intArray, numCheck)
