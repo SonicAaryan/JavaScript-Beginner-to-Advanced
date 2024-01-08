e25a3de9abc6e82885329203aef75387ceefdf4d
SonicAaryan <aaryansonar2@gmail.com>
2024-01-08 21:44:41 +0530

    Reverted Through --soft HEAD~1

--- Program39_FlattenAnArray.js
+++ Program39_FlattenAnArray.js
@@ -1,13 +1,12 @@
-let array=[1,[2,[3,4],5,6]];
-let res=[];
-let flatternAnArray=(array)=>{
-	for(let value of array){
-		if(Array.isArray(value)){
-			flatternAnArray(value);
-		}else{
-			res.push(value);
-		}
-	}
-}
-flatternAnArray(array);
-console.log(res);
+const PROMPT = require('prompt-sync')()
+//let length = parseInt(PROMPT("Enter the Length for Array :"));
+let newArray = [1, [2, [3, 4], 5]];
+// for (let arrayInsertion = 0; arrayInsertion < length; arrayInsertion++) {
+//     newArray[arrayInsertion] = PROMPT("Enter the Value :");
+// }
+console.log(typeof newArray);
+let newOne=[1,2,3,4,5];
+// console.log(newArray.length);
+// for (let i=0;i<newArray.length;i++) {
+//        newArray. 
+// }
