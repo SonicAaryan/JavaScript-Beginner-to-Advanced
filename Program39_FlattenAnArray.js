aab19ae068a93abbd4f1b9853dc61d0c80601b99
SonicAaryan <aaryansonar2@gmail.com>
2024-01-08 21:37:58 +0530

    Corrected the Code

--- Program39_FlattenAnArray.js
+++ Program39_FlattenAnArray.js
@@ -1,12 +1,13 @@
-const PROMPT = require('prompt-sync')()
-//let length = parseInt(PROMPT("Enter the Length for Array :"));
-let newArray = [1, [2, [3, 4], 5]];
-// for (let arrayInsertion = 0; arrayInsertion < length; arrayInsertion++) {
-//     newArray[arrayInsertion] = PROMPT("Enter the Value :");
-// }
-console.log(typeof newArray);
-let newOne=[1,2,3,4,5];
-// console.log(newArray.length);
-// for (let i=0;i<newArray.length;i++) {
-//        newArray. 
-// }
+let array=[1,[2,[3,4],5,6]];
+let res=[];
+let flatternAnArray=(array)=>{
+	for(let value of array){
+		if(Array.isArray(value)){
+			flatternAnArray(value);
+		}else{
+			res.push(value);
+		}
+	}
+}
+flatternAnArray(array);
+console.log(res);
