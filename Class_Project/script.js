573307261e571c4d107f52bb50bd0c1a212cc3a9
SonicAaryan <aaryansonar2@gmail.com>
2024-03-17 17:38:26 +0530

    Added Start_Btn and Pause_Btn Functionality

--- Class_Project/script.js
+++ Class_Project/script.js
@@ -4,26 +4,57 @@ let box = document.getElementById('whitebox');
 let p = document.getElementById('p');
 
 let start = document.getElementById('start');
-let stop = document.getElementById('stop');
+let pause = document.getElementById('stop');
 let restart = document.getElementById('restart');
 
-let i = 0;
-start.addEventListener('click', function (box) {
-    setInterval(function () {
-        p.innerHTML = ++i;
-        console.log(i);
-    }, 1000);
-});
 
-stop.addEventListener('click', function () {
-    setTimeout(function () {
-        p.innerHTML = i;
-        console.log('==================')
-        console.log(i)
-        console.log('==================')
-    }, 500);
+let temp;
+let seconds = 0;
+let hours = 0;
+let minute = 0;
+
+let timer = () => {
+    // return time;
+    if (seconds <= 59) {
+        // console.log(`${minute}: ${seconds++}`);
+        return `${hours} : ${minute} : ${seconds++}`
+    }
+    else {
+        if (minute >= 59) {
+            hours++;
+            minute = 0;
+        }
+        seconds = 0;
+        // console.log(` ${minute}: ${seconds++}`);
+        return `${hours} : ${minute++} : ${seconds++}`
+
+    }
+};
+let flag = false;
+start.addEventListener('click', function () {
+    if (flag == false) {
+        let id = setInterval(function () {
+            p.innerHTML = timer();
+            if (temp == 'pause') {
+                console.log('IN IF');
+                clearInterval(id);
+                flag = false;
+                temp = '';
+            }
+        }, 1000);
+    }
+    flag = true
+    // console.log(flag, "True");
 });
 
 restart.addEventListener('click', function () {
+    let restart = p.innerHTML = "00:00:00"
+    clearInInterval(restart);
 
 });
+
+pause.addEventListener('click', function () {
+    // clearInInterval();
+    temp = 'pause';
+});
+
